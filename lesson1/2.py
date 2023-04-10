ab2556d7024b3460aa8a4f9cb65d2edfd2289681
Сасмиль Кирилл <kir.sasmil2002@gmail.com>
2023-04-10 17:31:52 +0300

    w

--- lesson1/2.py
+++ lesson1/2.py
@@ -1,9 +1,9 @@
-print("part 2")
-word1 = b"class"
-word2 = b"function"
-word3 = b"method"
+#print("part 2")
+#word1 = b"class"
+#word2 = b"function"
+#word3 = b"method"
 
-print(type(word1), type(word2), type(word3))
-print(word1, word2, word3)
-print(len(word1), len(word2), len(word3))
-print('\n')
+#print(type(word1), type(word2), type(word3))
+#print(word1, word2, word3)
+#print(len(word1), len(word2), len(word3))
+#print('\n')
