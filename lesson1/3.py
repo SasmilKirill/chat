d1bfdf3c2267147331d84ec099a2d71f62686819
Сасмиль Кирилл <kir.sasmil2002@gmail.com>
2023-04-10 17:29:36 +0300

    w

---
+++ lesson1/3.py
@@ -0,0 +1,5 @@
+for s in ['attribute','класс','функция','type']:
+    try:
+        print(s,type(s),s.encode('ascii'), ' - encoding to bytes successful ')
+    except:
+        print(s,type(s),' - not valid input for bytes string')
---
+++ lesson1/6.py
@@ -0,0 +1,13 @@
+print(locale.getpreferredencoding())
+
+file = open("test_file.txt", "w")
+file.write("сетевое программирование\n")
+file.write("сокет\n")
+file.write("декоратор\n")
+
+print(file.encoding)
+file.close()
+
+with open("test_file.txt", encoding='utf-8') as file:
+    for line in file:
+        print(line, end='')
